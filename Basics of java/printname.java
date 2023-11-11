553363d7d6f61797ef2e8c559a79fd7492c08a49
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2023-11-11 11:54:09 +0530

    Printname.java file

---
+++ Basics of java/printname.java
@@ -0,0 +1,6 @@
+//Write a program to print your name on the screen
+public class printname{
+    public static void main(String[] args){
+        System.out.println("Mohammad Tahzeeb Khan");
+    }
+}
