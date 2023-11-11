5950678f924bdb2771fafb97cd15f4adac161029
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2023-11-11 11:58:01 +0530

    addtwono.java file

---
+++ Basics of java/addtwono.java
@@ -0,0 +1,10 @@
+//Write a program to add two nos. 
+public class addtwono{
+    public static void main(String[] args){
+        float a,b,c;
+        a=45;
+        b=3;
+        c=a+b;
+        System.out.println("Addition of two nos is: "+c);
+    }
+}
