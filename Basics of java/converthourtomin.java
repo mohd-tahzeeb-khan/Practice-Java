bcaf2dc861a1f1b704a54c1357ced83981d293e8
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2023-11-12 11:35:40 +0530

    code file and class file

---
+++ Basics of java/converthourtomin.java
@@ -0,0 +1,11 @@
+public class converthourtomin {
+    public static void main(String args[]){
+        if(args.length==1){
+            int hr=Integer.parseInt(args[0]);
+            int min=hr*60;
+            System.out.println(hr+ " hours are "+min+" Mins");
+        }else{
+            System.out.println("Please enter correct argument");
+        }
+    }
+}
