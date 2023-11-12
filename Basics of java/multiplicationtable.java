563b3abe33654e31923980b7f11aca46c99d9ee6
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2023-11-12 12:23:36 +0530

    multiplication table code and class dile

---
+++ Basics of java/multiplicationtable.java
@@ -0,0 +1,10 @@
+public class multiplicationtable {
+    public static void main(String args[]){
+        if(args.length==1){
+            int no=Integer.parseInt(args[0]);
+            for(int i=0; i<10;i++){
+                System.out.println(no+" x "+i+" = "+i*no);
+            }
+        }
+    }
+}
