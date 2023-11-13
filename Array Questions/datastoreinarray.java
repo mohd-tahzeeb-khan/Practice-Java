d6057e9288d16e2ebc2678722909d4ff3bbe1338
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2023-11-13 10:11:13 +0530

    data storein array

---
+++ Array Questions/datastoreinarray.java
@@ -0,0 +1,11 @@
+public class datastoreinarray{
+    public  static void main(String args[]){
+        int no[]=new int[11];
+        for(int i=1; i<=10; i++){
+            no[i]=i;
+        }
+        for(int i=1;i<=10;i++){
+            System.out.println(no[i]);
+        }
+    }
+}
